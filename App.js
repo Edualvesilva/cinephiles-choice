01c9a6fdb70f078a25c26b7853a0d8bad9974686
Eduardo Alves da Silva <eduardoalvniver@gmail.com>
2024-02-19 12:00:47 -0300

    Scrollview

--- App.js
+++ App.js
@@ -2,12 +2,14 @@ import { StatusBar } from "react-native";
 
 import Home from "./src/screens/Home";
 import About from "./src/screens/About";
+import Privacy from "./src/screens/Privacy";
 export default function App() {
   return (
     <>
       <StatusBar barStyle="light-content" />
       {/* <Home /> */}
-      <About />
+      {/* <About /> */}
+      <Privacy />
     </>
   );
 }
