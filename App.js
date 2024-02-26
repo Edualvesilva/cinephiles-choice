845caba227bb16072081a4e2ab2be4c9feecb0a4
Eduardo Alves da Silva <eduardoalvniver@gmail.com>
2024-02-26 09:18:08 -0300

    Config stack Navigation

--- App.js
+++ App.js
@@ -1,15 +1,23 @@
 import { StatusBar } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 import Home from "./src/screens/Home";
 import About from "./src/screens/About";
 import Privacy from "./src/screens/Privacy";
+
+// Initialization stack navigation mechanism
+const Stack = createNativeStackNavigator();
+
 export default function App() {
   return (
     <>
       <StatusBar barStyle="light-content" />
-      {/* <Home /> */}
-      {/* <About /> */}
-      <Privacy />
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={Home} />
+        </Stack.Navigator>
+      </NavigationContainer>
     </>
   );
 }
